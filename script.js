ce5ca5eb40c3bac9f78c2be98df3c44c98e0247d
Nazmul_Hossain <nazmulhossain2905@gmail.com>
2022-10-08 16:37:58 +0600

    removed comments

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+// variables
 let feet = document.getElementById("feet");
 let inches = document.getElementById("inches");
 
+// for inches
 feet.addEventListener("input", function () {
   let f = this.value;
   inches.value = f * 12;
 });
 
-// for inches
+// for feet
 inches.addEventListener("input", function () {
   let i = this.value;
   feet.value = !Number.isInteger(i / 12) ? (i / 12).toFixed(2) : i / 12;
